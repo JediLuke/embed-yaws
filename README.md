83fc2321469a3617ac1fb2e2734795c4d7b543e5
Luke Taylor <luke.taylor@heliansys.com>
2017-03-23 00:49:05 +1100

    Modifications to API and index.html

--- README.md
+++ README.md
@@ -5,7 +5,17 @@ Working example of embedding yaws inside a larger Erlang application. Also has a
 
 Note: This example is incompatible with windows ("There's no official support for rebar3 yet in yaws" 23/2/2016) - https://github.com/klacke/yaws/issues/254
 
-Build
------
+FreeBSD Build
+-------------
 
-    $ rebar3 compile
+See: https://www.digitalocean.com/community/tutorial_series/getting-started-with-freebsd?utm_source=Customerio&utm_medium=Email_Internal&utm_campaign=Email_FreeBSDDistroNginxWelcome&mkt_tok=eyJpIjoiTVRBNFlqUTFPRFppTWpoaSIsInQiOiJGSjJFaWFYUkRvSGUybjdPOUExc3VsZ2p5ekdDOUw0RXFqTElCcDRrbFVpT0pzZ2NKa2tvYVhlYUNzcURGTUxSS1ZURFVNY3NOWnoxYVUwYnJoN3JZWVwveDU2b1wvdlhEU2RUazhDa1lOVnhGR09Ja0tBK2VqSWx6ZWNvOVVpbnc2In0%3D
+See: https://www.digitalocean.com/community/tutorials/recommended-steps-for-new-freebsd-10-1-servers
+
+For some reason (build error) application:start(embed_yaws) fails, so manually starting it for now (this isn't meant to be a production release just an example code).
+
+    $ sudo pkg install git
+    $ sudo pkg install erlang
+    $ sudo pkg install rebar3
+
+    $ rebar3 shell
+    $ ybed_sup:start_link()
--- src/appmods/api.erl
+++ src/appmods/api.erl
@@ -1,25 +1,18 @@
 -module(api).
-
 -include_lib("yaws/include/yaws_api.hrl").
-%-include("../include/records.hrl").
 
 -export([out/1]).
 
--define(APIKEY, 123).
+
+%%% Usage of this appmod
+%%% the.server.ip.address:PORTNUM/api/params
 
 
 out(Arg) ->
     Uri = yaws_api:request_url(Arg),
     Method = (Arg#arg.req)#http_request.method,
     [ _BasePath | Params] = string:tokens(Uri#url.path, "/"),
-
-%    io:format("--------------~n"),
-%    %io:format("Arg: ~p~n", [Arg]),
-%    io:format("URI: ~p~n", [Uri]),
-%    io:format("Method: ~p~n", [Method]),
-%    io:format("Params: ~p~n", [Params]),
-%    io:format("--------------~n"),
-
+    % io:format("Arg: ~p~n", [Arg]),
     handle(Arg, Method, Params).
 
 
@@ -27,23 +20,8 @@ out(Arg) ->
 %% API handler methods
 %% -----------------------------------------------------------------
 
-%handle(_Arg, 'GET', []) ->
-%    [{status, 200},
-%     {html, "no args"}];
-
-
-handle(_Arg, 'GET', ["artilects"]) ->
-    %Devices = esb:list(devman, device, none, ?APIKEY),
-	timer:sleep(5000),
-    %%Struct = [convert_to_json(device, Device) || Device <- Devices],
-
-    [{status, 200}, {html, "List of All artilects"}];
-
 handle(_Arg, 'GET', ["test"]) ->
-    %Devices = esb:list(devman, device, none, ?APIKEY),
-    %%Struct = [convert_to_json(device, Device) || Device <- Devices],
-
-    [{status, 200}, {html, "Testttt"}];
+    [{status, 200}, {html, "Test HTML response."}];
 
 % handle(_Arg, 'GET', ["v1", "devices", D, "properties", P, "history"]) ->
 %     {DeviceId, _} = string:to_integer(D),
@@ -53,37 +31,6 @@ handle(_Arg, 'GET', ["test"]) ->
 %     Struct = [convert_to_json(datapoint, Datapoint) || Datapoint <- Datapoints],
 %
 %     [{status, 200}, {html, json2:encode(Struct)}];
-%
-%
-% handle(Arg, 'POST', ["v1", "devices", D, "properties", P ]) ->
-%     {DeviceId, _} = string:to_integer(D),
-%     {PropertyId, _} = string:to_integer(P),
-%     PostData = binary_to_list(Arg#arg.clidata),
-%
-% %    io:format("PostData: ~p~n", [PostData]),
-% %    io:format("Decoded: ~p~n", [json2:decode_string(PostData)]),
-%
-%     {ok, {struct, [Data]}} = json2:decode_string(PostData),
-%     {"value", Value} = Data,
-%
-%     Datapoint = esb:create_device_datapoint(DeviceId, PropertyId, Value, ?APIKEY),
-% %    io:format("Datapoint: ~p~n", [Datapoint]),
-%
-%     Struct = [convert_to_json(datapoint, Datapoint)],
-%
-%     [{status, 201}, {html, json2:encode(Struct)}];
-%
-% handle(Arg, 'GET', ["v1", "devices", D, "properties", P, V ]) ->
-%     {DeviceId, _} = string:to_integer(D),
-%     {PropertyId, _} = string:to_integer(P),
-%     {Value, _} = string:to_integer(V),
-%
-%     Datapoint = esb:create_device_datapoint(DeviceId, PropertyId, Value, ?APIKEY),
-%
-%     Struct = [convert_to_json(datapoint, Datapoint)],
-%
-%     [{status, 201}, {html, json2:encode(Struct)}];
-
 
 handle(_Arg, _Method, _Params) ->
     [{status, 404}, {html, "not found"}].
@@ -92,21 +39,3 @@ handle(_Arg, _Method, _Params) ->
 %% -----------------------------------------------------------------
 %% Helper methods
 %% -----------------------------------------------------------------
-% convert_to_json(device, Record) ->
-%     {
-%         struct,
-%         [
-%             {"id",          Record#device.id},
-%             {"name",        Record#device.name},
-%             {"description", Record#device.description}
-%         ]
-%     };
-%
-% convert_to_json(datapoint, Datapoint) ->
-%     {
-%         struct,
-%         [
-%            {"datetime", Datapoint#datapoint.datetime},
-%             {"value",    Datapoint#datapoint.value}
-%         ]
-%     }.
